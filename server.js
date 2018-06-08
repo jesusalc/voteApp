6c950ef7c1ebfc3f554facc2232e1199e5388714
jesusalc <jesusalc@gmail.com>
2018-06-08 18:10:27 +0200

    updated updated server to use model and routes

--- server.js
+++ server.js
@@ -3,44 +3,15 @@
  * For a more professional setup of Express, see...
  * http://expressjs.com/en/starter/generator.html
  */
-
 import express from 'express'
-import mongoose from 'mongoose'
-import processConnection from './app/lib/mongoose-processConnection'
 import bodyParser from 'body-parser'
 import http from 'http'
 import path from 'path'
+import mongoose from './app/middleware/obtain-connections'
 const app = express()
-
-import sayingsSchema from './app/db/schemas/sayingsSchema'
-const Sayings = processConnection('sayings', sayingsSchema)
-
-import usersSchema from './app/db/schemas/usersSchema'
-const Users = processConnection('users', usersSchema)
-
-import ratingsSchema from './app/db/schemas/ratingsSchema'
-const Ratings = processConnection('ratings', ratingsSchema)
-
-import initialSayings from './config/sayings.json';
+const router = app.Router()
 
 app.set('port', 3000);
-
-mongoose.Promise = global.Promise;
-let options = {
-  //useMongoClient: true,
-  autoIndex: false, // Don't build indexes
-  reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
-  reconnectInterval: 500, // Reconnect every 500ms
-  poolSize: 10, // Maintain up to 10 socket connections
-  // If not connected, return errors immediately rather than waiting for reconnect
-  bufferMaxEntries: 0
-};
-mongoose.connect(process.env.MONGO_URL, options);
-// mongoose.connect('mongodb://localhost/voteApp');
-/**
- * Anything in public can be accessed statically without
- * this express router getting involved
- */
 
 const allowCors = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -54,85 +25,9 @@ app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname + '/app/build')));
 
-const findUserByName = (name) => {
-   return new Promise((resolve, reject) => {
-       resolve(Users.findOne({ name : name }, '_id name', (err, user) => {
-           return user;
-       }))
-   });
-};
+const { findUserByName, findRatingsofUser } = require('../app/models');
 
-const findRatingsofUser = (userId) => {
-    return new Promise((resolve, reject) => {
-        resolve(
-            Ratings.find({}).select('-_id saying').where('user').equals(userId).exec((err, ratings) => {
-                return ratings;
-            })
-        )
-    })
-};
-
-
-app.get('/sayings/:user', function (req, res) {
-    res.sendFile(path.join(__dirname + '/app/build/index.html'));
-});
-
-/**
- * Always serve the same HTML file for all requests
- */
-
-app.get('/initializeSayings', function(req, res) {
-    Sayings.insertMany(initialSayings.sayings, (err, document) => {
-        res.send(err)
-    });
-});
-
-app.get('/api/user', function (req, res) {
-    findUserByName(req.query.name)
-        .then(foundUser => {
-            return foundUser
-        }).then(user => {
-            if (user === null) {
-                const user = new Users({
-                    name: req.query.name
-                });
-                return user.save().then(savedUser => {
-                    return savedUser;
-                })
-            }
-            return user;
-    }).then(user => {
-        res.send(user);
-    });
-});
-
-app.get('/api/sayings', function (req, res, next) {
-    findRatingsofUser(req.query.userId)
-        .then((sayings) => {
-            const sayingIds = sayings.map((saying) => {
-                return saying.saying;
-            });
-            Sayings.find({})
-            .where('_id').nin(sayingIds)
-                .limit(25)
-                .exec(function (err, sayings) {
-
-                    res.json({sayings, totalVoted: sayingIds.length});
-                    res.end();
-                });
-        });
-
-});
-
-app.post('/api/rateSaying', function (req,res) {
-    const rating = new Ratings({
-        rate: req.body.rate,
-        user: req.body.user,
-        saying: req.body.sayingId
-    });
-    rating.save();
-    res.end();
-});
+require('./app/routes')(router);
 
 /**
  * Start Server
