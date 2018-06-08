334430df230a327a1834418722e54c42446d0929
jesusalc <jesusalc@gmail.com>
2018-06-08 19:36:18 +0200

    corrected corrected and added more routes and user init route and routes

--- app/routes.js
+++ app/routes.js
@@ -1,21 +1,58 @@
 'use strict'
 import path from 'path'
 module.exports = (router, schemas, models)  => {
-    router.get('/sayings/:user', (req, res) => {
-        res.sendFile(path.join(__dirname + '/build/index.html'))
-    })
+
 
     /**
      * Always serve the same HTML file for all requests
      */
+    router.get('/routes', (req, res) => {
+        const temp_head = '<!DOCTYPE html> \
+                            <html lang="en"> \
+                            <head> \
+                                <meta charset="UTF-8"> \
+                                <title>Routes</title> \
+                                <link rel="stylesheet" href="/css/styles.css"> \
+                            </head> \
+                            <body> \
+                            <div id="root"></div> \
+                            '
+        const routes =  [
+                            {"route" : '<a href="http://127.0.0.1:3000/">home</a>'},
+                            {"route" : '<a href="http://127.0.0.1:3000/initializeCollections">init db</a>'},
+                            {"route" : '<a href="http://127.0.0.1:3000/api/user">api/user</a>'},
+                            {"route" : '<a href="http://127.0.0.1:3000/api/user">api/user</a>'},
+                            {"route" : '<a href="http://127.0.0.1:3000/api/sayings">api/sayings</a>'},
+                            {"route" : '<a href="http://127.0.0.1:3000/api/rateSaying">api/rateSaying</a>'},
+                        ]
 
-    router.get('/initializeSayings', (req, res) => {
+        const temp_body = routes.map((route) => {
+                return route.route + '<br/> \\'
+            })
+        const temp_footer = '</body> \
+                            </html>'
+        const temp_html = temp_head + temp_body + temp_footer
+        res.send( temp_html )
+    })
+
+    router.get('/initializeCollections', (req, res) => {
         const initialSayings = require('../config/sayings.json')
 
         schemas.Sayings.insertMany(
             initialSayings.sayings, (err, document) => {
             res.send(err)
         })
+
+        const initialUsers = require('../config/users.json')
+
+        schemas.Users.insertMany(
+            initialUsers.users, (err, document) => {
+            res.send(err)
+        })
+    })
+
+    router.get('/sayings/:user', (req, res) => {
+        res.sendFile(path.join(__dirname + '/build/index.html'))
     })
 
     router.get('/api/user', (req, res) => {
